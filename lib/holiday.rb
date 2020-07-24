d68791a619420e3ef8d050bf8754db259bfe0b9c
Leslie G <lesliegonzalez@me.com>
2020-07-24 04:29:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,6 +40,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holidays, supplies|
       holiday if supplies.include?("BBQ")
+    end
     
   
   # return an array of holiday names (as symbols) where supply lists
